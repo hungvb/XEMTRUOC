d39035d505653ae8528198544b46f328a9ba1eb9
Hung Visual Basic <89638213+hungvb@users.noreply.github.com>
2022-06-11 22:14:48 +0700

    Add files via upload

---
+++ 160x600-ads.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'f45512bd1119fdb53c2ca412aba84046',
+		'format' : 'iframe',
+		'height' : 600,
+		'width' : 160,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.topdisplayformat.com/f45512bd1119fdb53c2ca412aba84046/invoke.js"></scr' + 'ipt>');
